cc2a429b7793dec739a86fd34c3563c5f5c21195
aaliawadkar1211 <aaliasw0207@gmail.com>
2025-09-20 18:46:18 +0530

    Initial Commit

---
+++ MediSnap/MediSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediSnapApp.swift
+//  MediSnap
+//
+//  Created by Salman Mhaskar on 20/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MediSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
